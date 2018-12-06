eb295376ff067809f3c323617ae573b8b7e8783f
c811k <bhk811@gmail.com>
2018-12-05 17:21:06 -0800

    working version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,4 @@
-var topics = ["kobe bryant", "magic johnson", "jason kidd", "steve nash", "lebron james", "kyrie irving", "james harden", "russell westbrook", "paul george", "carmelo anthony", "anthony davis", "stephen curry", "kevin durant"];
-
-
+var topics = ["kobe bryant", "magic johnson", "jason kidd", "steve nash", "lebron james", "kyrie irving", "james harden", "russell westbrook", "paul george", "carmelo anthony", "anthony davis", "stephen curry", "kevin durant", "giannis Antetokounmpo", "joel embiid", "john wall", "chris paul", "blake griffin", "jimmy butler", "damian lillard", "kawhi leonard", "kemba walker"];
 
 function display() {
     $("#gifs-view").empty();
@@ -12,25 +10,50 @@ function display() {
         method: "GET"
     }).then(function(response) {
         console.log(response);
-        for(var i = 0; i < 10; i++) {
-            var image = $("<img>").attr("src", response.data[i].images.fixed_height.url);
-            $("#gifs-view").append(image);
-            var rating = $("<p>").text("Rating: " + response.data[i].rating);
-            $("#gifs-view").append(rating);
+        var results = response.data;
+
+        for(var i = 0; i < results.length; i++) {
+            var topicDiv = $("<div>");
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            var image = $("<img>")
+                .addClass("gif position-static")
+                .attr("src", results[i].images.fixed_width_still.url)
+                .attr("data-state", "still")
+                .attr("data-animate", results[i].images.fixed_width.url)
+                .attr("data-still", results[i].images.fixed_width_still.url);
+            topicDiv.append(p, image);
+            $("#gifs-view").append(topicDiv);
         }
+
+        $(".gif").on("click", function() {
+            var state = $(this).attr("data-state");
+            console.log(state);
+            
+            if(state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            }
+            else if(state === "animate") {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        
+        });
     });
 }
 
 function renderButtons() {
     $("#topics-view").empty();
     for (var i = 0; i < topics.length; i++) {
-        var btn = $("<button>");
-        btn.addClass("btn btn-info");
+        var btn = $("<button id='click-me'>");
+        btn.addClass("btn btn-info btn-sm");
         btn.attr("data-name", topics[i]);
         btn.text(topics[i]);
         $("#topics-view").append(btn);
     }
 }
+
+
 
 $("#add-topic").on("click", function(event) {
     event.preventDefault();
@@ -39,5 +62,5 @@ $("#add-topic").on("click", function(event) {
     renderButtons();
 });
 
-$(document).on("click", ".btn", display);
+$(document).on("click", ".btn.btn-info", display);
 renderButtons();
